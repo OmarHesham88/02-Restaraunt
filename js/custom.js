94adb071ddd455ffac2d2431f2a436309c47ff64
omarhesham88 <omarhisham844@gmail.com>
2026-02-09 23:36:10 +0200

    Refine navbar toggle behavior and sticky/menu visual fixes

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,20 @@
 
-  (function ($) {
-  
+(function ($) {
   "use strict";
 
-    // NAVBAR
-    $('.navbar-nav .nav-link').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
-    
-  })(window.jQuery);
+  // NAVBAR
+  $(".navbar-nav .nav-link").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  // Handle navbar toggle with Bootstrap collapse events
+  $("#navbarNav").on("show.bs.collapse", function () {
+    // Menu is opening - only hide logo, don't move anything
+    $(".navbar-brand").addClass("hide-logo");
+  });
+
+  $("#navbarNav").on("hidden.bs.collapse", function () {
+    // Menu is closed - show logo again
+    $(".navbar-brand").removeClass("hide-logo");
+  });
+})(window.jQuery);
