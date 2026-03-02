e882e0a2e4e1ed9559d842628322621e9d917d0d
omarhesham88 <omarhisham844@gmail.com>
2026-03-03 00:04:56 +0200

    Update cart and custom script changes

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,3 @@
-
 (function ($) {
   "use strict";
 
@@ -17,4 +16,44 @@
     // Menu is closed - show logo again
     $(".navbar-brand").removeClass("hide-logo");
   });
+
+  function evaluateMenuImageQuality(img) {
+    var wrap = img.closest(".menu-image-wrap");
+    if (!wrap || !img.naturalWidth || !img.naturalHeight) return;
+
+    var wrapWidth = wrap.clientWidth;
+    var wrapHeight = wrap.clientHeight;
+    if (!wrapWidth || !wrapHeight) return;
+    var isLowRes =
+      img.naturalWidth < wrapWidth || img.naturalHeight < wrapHeight;
+
+    img.classList.toggle("menu-image-low-res", isLowRes);
+    wrap.classList.toggle("menu-image-wrap-low-res", isLowRes);
+  }
+
+  function initMenuImageFallback() {
+    var menuImages = document.querySelectorAll(".menu-image-wrap .menu-image");
+    if (!menuImages.length) return;
+
+    function refreshAll() {
+      menuImages.forEach(function (img) {
+        evaluateMenuImageQuality(img);
+      });
+    }
+
+    menuImages.forEach(function (img) {
+      if (img.complete) {
+        evaluateMenuImageQuality(img);
+      } else {
+        img.addEventListener("load", function () {
+          evaluateMenuImageQuality(img);
+        });
+      }
+    });
+
+    window.addEventListener("load", refreshAll);
+    window.addEventListener("resize", refreshAll);
+  }
+
+  initMenuImageFallback();
 })(window.jQuery);
